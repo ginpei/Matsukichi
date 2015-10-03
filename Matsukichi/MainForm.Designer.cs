2c187e2d2a577c3be62fb02514059a576faa9bf6
ginpei <ginpei@gmail.com>
2015-10-03 13:03:40 -0700

    just save "MainForm.cs [Design]" ...

--- Matsukichi/MainForm.Designer.cs
+++ Matsukichi/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Matsukichi
+namespace Matsukichi
 {
     partial class MainForm
     {
@@ -39,9 +39,9 @@
             this.uiFilterText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.uiFilterText.Font = new System.Drawing.Font("MS UI Gothic", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.uiFilterText.Location = new System.Drawing.Point(68, 12);
+            this.uiFilterText.Location = new System.Drawing.Point(61, 13);
             this.uiFilterText.Name = "uiFilterText";
-            this.uiFilterText.Size = new System.Drawing.Size(673, 51);
+            this.uiFilterText.Size = new System.Drawing.Size(606, 51);
             this.uiFilterText.TabIndex = 0;
             this.uiFilterText.TextChanged += new System.EventHandler(this.uiFilterText_TextChanged);
             this.uiFilterText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.uiFilterText_KeyDown);
@@ -52,27 +52,27 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.uiCommandList.FormattingEnabled = true;
-            this.uiCommandList.ItemHeight = 18;
-            this.uiCommandList.Location = new System.Drawing.Point(12, 69);
+            this.uiCommandList.ItemHeight = 20;
+            this.uiCommandList.Location = new System.Drawing.Point(11, 77);
             this.uiCommandList.Name = "uiCommandList";
-            this.uiCommandList.Size = new System.Drawing.Size(729, 94);
+            this.uiCommandList.Size = new System.Drawing.Size(656, 104);
             this.uiCommandList.TabIndex = 1;
             this.uiCommandList.SelectedIndexChanged += new System.EventHandler(this.uiCommandList_SelectedIndexChanged);
             // 
             // uiIconPlace
             // 
             this.uiIconPlace.ImageLocation = "";
-            this.uiIconPlace.Location = new System.Drawing.Point(12, 12);
+            this.uiIconPlace.Location = new System.Drawing.Point(11, 13);
             this.uiIconPlace.Name = "uiIconPlace";
-            this.uiIconPlace.Size = new System.Drawing.Size(50, 50);
+            this.uiIconPlace.Size = new System.Drawing.Size(45, 56);
             this.uiIconPlace.TabIndex = 2;
             this.uiIconPlace.TabStop = false;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(753, 175);
+            this.ClientSize = new System.Drawing.Size(678, 194);
             this.Controls.Add(this.uiIconPlace);
             this.Controls.Add(this.uiFilterText);
             this.Controls.Add(this.uiCommandList);
